0614806bcb20158ce3de6d9c439f83b150c24d09
Pierre caillous <Pierre.cailloupgmail.com>
2026-02-05 17:26:12 +0100

    add mvc layers

---
+++ spotify/src/main/java/com/geese/spotify/controller/SearchController.java
@@ -0,0 +1,25 @@
+package com.geese.spotify.controller;
+
+import com.geese.spotify.model.Video;
+import com.geese.spotify.service.YoutubeService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/videos")
+public class SearchController {
+    private final YoutubeService youtubeService;
+
+    public SearchController(YoutubeService youtubeService) {
+        this.youtubeService = youtubeService;
+    }
+
+    @GetMapping("/search")
+    public List<Video> searchVideos(@RequestParam String query) {
+        return youtubeService.searchVideos(query);
+    }
+}
--- spotify/src/main/java/com/geese/spotify/service/YoutubeService.java
+++ spotify/src/main/java/com/geese/spotify/service/YoutubeService.java
@@ -1,13 +1,55 @@
 package com.geese.spotify.service;
 
 import com.geese.spotify.model.Video;
-import com.google.api.client.util.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+import tools.jackson.databind.JsonNode;
+import tools.jackson.databind.ObjectMapper;
+
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class YoutubeService {
     @Value("${youtube.api.key}")
     private String apiKey;
+    private static final long numberOfSearches = 2;
+    private final String YOUTUBE_URL = "https://www.googleapis.com/youtube/v3/search";
 
+    public List<Video> searchVideos(String query){
+        RestTemplate restTemplate = new RestTemplate();
+        List<Video> videoList = new ArrayList<>();
+
+        String url = UriComponentsBuilder.fromUriString(YOUTUBE_URL)
+                .queryParam("part", "snippet")
+                .queryParam("q", query)
+                .queryParam("maxResults", numberOfSearches)
+                .queryParam("type", "video")
+                .queryParam("key", apiKey)
+                .build()
+                .toUriString();
+
+        try {
+            //api call
+            String response = restTemplate.getForObject(url, String.class);
+
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode root = mapper.readTree(response);
+            JsonNode results = root.get("item");
+
+            for (JsonNode result : results) {
+                Video video = new Video();
+                video.setTitle(result.get("title").asText());
+                video.setVideoId(result.get("id").asText());
+                videoList.add(video);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return videoList;
+    }
 
 }
